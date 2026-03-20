ce8d933254db764ec5e58ca093fa54da1ced8c19
jerinmt <jerinmt379@gmail.com>
2026-03-20 21:57:18 +0530

    Changed the routing for express

--- server/stylehub/routes/indexRouter.js
+++ server/stylehub/routes/indexRouter.js
@@ -3,15 +3,16 @@ const adjustController = require("../controllers/adjustController");
 const fileController = require("../controllers/fileController");
 const inputController = require("../controllers/inputController");
 const outputController = require("../controllers/outputController");
-const wakeController = require("../controllers/wakeController");
+const indexController = require("../controllers/indexController");
 
 const indexRouter = Router();
 
-indexRouter.get("/", wakeController.wakeUp);
+indexRouter.get("/", indexController.wakeUp);
 indexRouter.post("/initiate", inputController.initiatePrompt);
-indexRouter.get("/preview", outputController.showOutput);
-indexRouter.post("/adjust", adjustController.adjust);
-indexRouter.get("/file", fileController.fileDownload);
-indexRouter.post("/repeat", inputController.repeatStyle);
+indexRouter.post("/preview", outputController.previewBuild);
+indexRouter.post("/adjust", adjustController.adjustPrompt);
+indexRouter.post("/file", fileController.fileDownload);
+indexRouter.post("/repeat", inputController.repeatPrompt);
+indexRouter.get("/reset", indexController.resetPrompt);
 
 module.exports = indexRouter;
