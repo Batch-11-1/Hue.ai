85492d3e2aa62cea817f5ff9b6d7d69cadc20ffd
renjeesh-rs <vichurenjeesh@gmail.com>
2026-03-30 14:48:37 +0530

    completed intialPrompt

--- server/stylehub/app.js
+++ server/stylehub/app.js
@@ -6,8 +6,10 @@ const indexRouter = require("./routes/indexRouter");
 //initialisations
 const app = express();
 
-//for form values
-app.use(express.urlencoded({extended: true}));
+// For request bodies (either form-urlencoded or JSON).
+// HTML files can be large, so we increase limits from Express defaults.
+app.use(express.urlencoded({ extended: true, limit: "5mb" }));
+app.use(express.json({ limit: "5mb" }));
 
 //routing
 app.use("/", indexRouter);
