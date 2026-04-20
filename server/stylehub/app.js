f32d08b3c7f9539ebc044c1781818f91bc70bdcc
jerinmt <jerinmt379@gmail.com>
2026-04-20 20:15:40 +0530

    Debuggin cors implementation

--- server/stylehub/app.js
+++ server/stylehub/app.js
@@ -37,5 +37,5 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}!`);
+  console.log(`Server listening on port ${PORT}! From ${process.env.FRONTEND_URL}`);
 });
