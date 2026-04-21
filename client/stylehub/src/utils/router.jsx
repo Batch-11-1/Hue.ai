935ac39c3bfc9e0a59ac4b71e8b64ec1846a77e5
jerinmt <jerinmt379@gmail.com>
2026-04-21 13:19:51 +0530

    scroll bug fix

--- client/stylehub/src/utils/router.jsx
+++ client/stylehub/src/utils/router.jsx
@@ -5,14 +5,20 @@ import Input from "../pages/Input"
 import Output from "../pages/Output"
 import Result from "../pages/Result"
 import ErrorPage from "../pages/ErrorPage"
+import Layout from "../components/Layout";
 
 const router = createBrowserRouter([
-    { path: '', element: <App/>, errorElement: <ErrorPage/> },
-    { path : '/aboutus' , element : <AboutUs/> },
-    { path : '/input', element: <Input/>},
-    { path : '/output', element: <Output/>},
-    { path : '/result', element: <Result/>},
-    { path: '*', element: <ErrorPage /> }
+    {
+        element: <Layout />,
+        children: [
+            { path: '', element: <App/>, errorElement: <ErrorPage/> },
+            { path : '/aboutus' , element : <AboutUs/> },
+            { path : '/input', element: <Input/>},
+            { path : '/output', element: <Output/>},
+            { path : '/result', element: <Result/>},
+            { path: '*', element: <ErrorPage /> }
+        ]
+    }
 ]);
 
 export default router;
